1d1f667aa0f6615f91b8456d5ed04c323d274b37
Crowdbotics <team@crowdbotics.com>
2021-08-24 14:24:08 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "throbbing_cell_30070Identifier",
+  url: "https://throbbing_cell_30070Identifier.botics.co",
+  api: "https://throbbing_cell_30070Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
